12f9517d098f1216065b4abccb7f8c41fd3239d9
sreey <sreelakshmi12898@gmail.com>
2023-06-28 19:51:12 +0530

    Change states in class based components

--- src/components/userList/Users.jsx
+++ src/components/userList/Users.jsx
@@ -1,35 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import styles from './Users.module.css';
 import User from "./User";
 
-const DUMMY_USERS = [
-    {id: 'u1', name: 'Shay'},
-    {id: 'u2', name: 'Robin'},
-    {id: 'u3', name: 'Ray'}
-];
-
-function Users(){
-
-    const [showUsers, setShowUsers] = useState(false);
-
-    const getUsers = () => {
-        setShowUsers(prevState => !prevState)
+class Users extends Component{
+    
+    constructor(){
+        super();
+        this.state = {
+            showUsers: false
+        };
     }
 
-    const userList =   <ul>
-    {DUMMY_USERS.map(user => 
-        <User key={user.id} name={user.name} />
-        )}
-</ul>
+    getUsers(){
+        this.setState((prevState) => {
+            return {showUsers: !prevState.showUsers}
+        })
+    }
 
-    return(
-        <div className={styles.users} >
-            <button onClick={getUsers} >
-                {showUsers ? 'Hide' : 'Show'} Users
-            </button>
-          {showUsers && userList}
-        </div>
-    );
-};
+    render(){
+
+        const DUMMY_USERS = [
+            {id: 'u1', name: 'Shay'},
+            {id: 'u2', name: 'Robin'},
+            {id: 'u3', name: 'Ray'}
+        ];
+
+        const userList =   <ul>
+        {DUMMY_USERS.map(user => 
+            <User key={user.id} name={user.name} />
+            )}
+    </ul>
+
+        return(
+            <div className={styles.users} >
+                <button onClick={this.getUsers.bind(this)} >
+                    {this.state.showUsers ? 'Hide' : 'Show'} Users
+                </button>
+              {this.state.showUsers && userList}
+            </div>
+        );
+    }
+}
+
+
+
+// function Users(){
+
+//     const [showUsers, setShowUsers] = useState(false);
+
+//     const getUsers = () => {
+//         setShowUsers(prevState => !prevState)
+//     }
+
+   
+
+//     return(
+//         <div className={styles.users} >
+//             <button onClick={getUsers} >
+//                 {showUsers ? 'Hide' : 'Show'} Users
+//             </button>
+//           {showUsers && userList}
+//         </div>
+//     );
+// };
 
 export default Users;
