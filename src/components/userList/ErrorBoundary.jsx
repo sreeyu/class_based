9c7057588c13b3c6c4f2dae15c6897892efa7c14
sreey <sreelakshmi12898@gmail.com>
2023-06-30 23:23:02 +0530

    Style error message

--- src/components/userList/ErrorBoundary.jsx
+++ src/components/userList/ErrorBoundary.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styles from './ErrorBoundary.module.css'
 
 class ErrorBoundary extends Component{
     constructor(){
@@ -15,7 +16,7 @@ class ErrorBoundary extends Component{
 
     render() {
         if (this.state.hasError){
-            return <p>Something went wrong!</p>
+            return <p className={styles.error}>Something went wrong!</p>
         }
         return this.props.children;
     };
